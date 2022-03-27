2e5a6d7d80b97bc980d8453723782d32eb85745d
includingivansmirnov@gmail.com <includingivansmirnov@gmail.com>
2022-03-27 15:46:49 +0500

    Add: Base view-model

---
+++ CV19/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CV19.View.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
