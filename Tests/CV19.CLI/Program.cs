1dfe8e08a768e806786cace48a875c781f5947cf
includingivansmirnov@gmail.com <includingivansmirnov@gmail.com>
2022-03-27 14:46:43 +0500

    Add: Тестовый консольный проект

---
+++ Tests/CV19.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CV19.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
