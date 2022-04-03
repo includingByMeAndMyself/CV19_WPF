5900ed0a3b02a1adf07a7d7c8149199967dc8d34
includingivansmirnov@gmail.com <includingivansmirnov@gmail.com>
2022-04-03 17:16:16 +0500

    Add: TestData for models for graph

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CV19.Infrastucture.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,6 +10,18 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region TestDataPoints
+
+        private IEnumerable<DataPoint> _testDataPoint;
+
+        public IEnumerable<DataPoint> TestDataPoint 
+        { 
+            get => _testDataPoint;
+            set => Set(ref _testDataPoint, value);
+        }
+
+        #endregion
+
         #region Title : string Заголовок окна
 
         private string _title = "Анализ статистики CV19";
@@ -59,6 +74,16 @@ namespace CV19.ViewModels
             CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
             #endregion
+
+            var dataPoints = new List<DataPoint>((int)(360/0.1));
+            for(var x = 0d; x <=360; x += 0.1)
+            {
+                const double toRad = Math.PI / 180;
+                var y = Math.Sin(x * toRad);
+
+                dataPoints.Add(new DataPoint { XValue = x, YValue = y });
+            }
+            TestDataPoint = dataPoints;
         }
     }
 }
